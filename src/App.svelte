ec054593ba5f2e75c2b2b05f72a106a828eae363
Kho Kho <khokho@localhost.localdomain>
2019-08-10 08:10:40 +0700

    re-indenting stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,55 @@
 <script>
-  import Header from "./UI/Header.svelte";
-  import Grid from "./Meetups/Grid.svelte";
-  import TextInput from "./UI/TextInput.svelte";
-  import Button from "./UI/Button.svelte";
-  import EditMeetup from "./Meetups/EditMeetup.svelte";
-  import meetups from "./stores/meetups";
-  import Details from "./Meetups/Details.svelte";
+	import Header from "./UI/Header.svelte";
+	import Grid from "./Meetups/Grid.svelte";
+	import TextInput from "./UI/TextInput.svelte";
+	import Button from "./UI/Button.svelte";
+	import EditMeetup from "./Meetups/EditMeetup.svelte";
+	import meetups from "./stores/meetups";
+	import Details from "./Meetups/Details.svelte";
 
-  let editMode = null;
-  let currentPage = "overview";
-  let id = null;
-  let editedId;
+	let editMode = null;
+	let currentPage = "overview";
+	let id = null;
+	let editedId;
 
-  function close() {
-    resetMode();
-  }
+	function close() {
+		resetMode();
+	}
 
-  function showDetails({ detail }) {
-    id = detail;
-  }
+	function showDetails({ detail }) {
+		id = detail;
+	}
 
-  function deleteId() {
-    id = null;
-  }
+	function deleteId() {
+		id = null;
+	}
 
-  function edit({ detail }) {
-    editedId = detail;
-    editMode = true;
-  }
+	function edit({ detail }) {
+		editedId = detail;
+		editMode = true;
+	}
 
-  function resetMode() {
-    editMode = null;
-    editedId = null;
-  }
+	function resetMode() {
+		editMode = null;
+		editedId = null;
+	}
 </script>
 
 <style>
-  main {
-    margin-top: 5rem;
-  }
+	main {
+		margin-top: 5rem;
+	}
 </style>
 
 <Header />
 
 <main>
-  {#if id === null}
-    {#if editMode}
-      <EditMeetup on:close={close} id={editedId} />
-    {/if}
-    <Grid meetups={$meetups} on:showdetails={showDetails} on:edit={edit} />
-  {:else}
-    <Details {id} on:close={deleteId} />
-  {/if}
+	{#if id === null}
+		{#if editMode}
+			<EditMeetup on:close={close} id={editedId} />
+		{/if}
+		<Grid meetups={$meetups} on:showdetails={showDetails} on:edit={edit} />
+	{:else}
+		<Details {id} on:close={deleteId} />
+	{/if}
 </main>
--- src/stores/meetups.js
+++ src/stores/meetups.js
@@ -2,66 +2,66 @@ import { writable } from 'svelte/store';
 
 // private store data, don't bother
 const _meetups = writable([
-    {
-        id: "m1",
-        title: "Coding Bootcamp",
-        subtitle: "Learn in 2 hours",
-        description: "Sponsored by United Nations",
-        imageUrl:
-            "https://i2.wp.com/www.animemaru.com/wp-content/uploads/2018/02/catgirls_nation_research.jpg?resize=640%2C360&ssl=1",
-        address: "8 Nirvana Street",
-        contactEmail: "code@test.com"
-    },
-    {
-        id: "m2",
-        title: "Swimming Catgirls",
-        subtitle: "Let's fricking go",
-        description: "Swimming lessons with other fellow catgirls",
-        imageUrl:
-            "https://www.wallpaperup.com/uploads/wallpapers/2014/07/01/383259/0e6ce91ca8a334ea9636315ac2c94995-700.jpg",
-        address: "UN Swimming Pool, Room 2",
-        contactEmail: "swim@test.com"
-    }
+	{
+		id: "m1",
+		title: "Coding Bootcamp",
+		subtitle: "Learn in 2 hours",
+		description: "Sponsored by United Nations",
+		imageUrl:
+			"https://i2.wp.com/www.animemaru.com/wp-content/uploads/2018/02/catgirls_nation_research.jpg?resize=640%2C360&ssl=1",
+		address: "8 Nirvana Street",
+		contactEmail: "code@test.com"
+	},
+	{
+		id: "m2",
+		title: "Swimming Catgirls",
+		subtitle: "Let's fricking go",
+		description: "Swimming lessons with other fellow catgirls",
+		imageUrl:
+			"https://www.wallpaperup.com/uploads/wallpapers/2014/07/01/383259/0e6ce91ca8a334ea9636315ac2c94995-700.jpg",
+		address: "UN Swimming Pool, Room 2",
+		contactEmail: "swim@test.com"
+	}
 ])
 
 // exposing a bunch of public methods
 const meetups = {
-    subscribe: _meetups.subscribe,
-    newMeetup: ({ title, subtitle, contactEmail, address, description, imageUrl }) => {
-        const meetupData = {
-            title,
-            subtitle,
-            contactEmail,
-            address,
-            description,
-            imageUrl,
-            id: Math.random().toString(),
-            isFavorite: false
-        }
-        _meetups.update(items => [meetupData, ...items]);
-    },
-    toggleFavorite: (id) => {
-        _meetups.update(items => {
-            const meetup = items.find(m => m.id === id);
-            if (meetup) {
-                meetup.isFavorite = !meetup.isFavorite
-            }
-            return items;
-        })
-    },
-    editMeetup: (id, { title, subtitle, contactEmail, address, description, imageUrl }) => {
-        _meetups.update(items => {
-            const meetupIndex = items.findIndex(m => m.id === id);
-            if (meetupIndex !== -1) {
-                const editedMeetup = { id, title, subtitle, contactEmail, address, description, imageUrl };
-                items[meetupIndex] = editedMeetup;
-            }
-            return items;
-        })
-    },
-    removeMeetup: (id) => {
-        _meetups.update(items => items.filter(m => m.id !== id));
-    }
+	subscribe: _meetups.subscribe,
+	newMeetup: ({ title, subtitle, contactEmail, address, description, imageUrl }) => {
+		const meetupData = {
+			title,
+			subtitle,
+			contactEmail,
+			address,
+			description,
+			imageUrl,
+			id: Math.random().toString(),
+			isFavorite: false
+		}
+		_meetups.update(items => [meetupData, ...items]);
+	},
+	toggleFavorite: (id) => {
+		_meetups.update(items => {
+			const meetup = items.find(m => m.id === id);
+			if (meetup) {
+				meetup.isFavorite = !meetup.isFavorite
+			}
+			return items;
+		})
+	},
+	editMeetup: (id, { title, subtitle, contactEmail, address, description, imageUrl }) => {
+		_meetups.update(items => {
+			const meetupIndex = items.findIndex(m => m.id === id);
+			if (meetupIndex !== -1) {
+				const editedMeetup = { id, title, subtitle, contactEmail, address, description, imageUrl };
+				items[meetupIndex] = editedMeetup;
+			}
+			return items;
+		})
+	},
+	removeMeetup: (id) => {
+		_meetups.update(items => items.filter(m => m.id !== id));
+	}
 }
 
 export default meetups;
